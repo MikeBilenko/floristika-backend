36480932aaed3d969eab4aebe2800dc59c115689
mike <mike@cdontiy.com>
2024-04-16 17:52:27 +0300

    added trusted origin

--- floristika_backend/settings/local.py
+++ floristika_backend/settings/local.py
@@ -9,13 +9,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["24.144.76.8", ]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://localhost:3000",
-    "http://24.144.76.8:8080",
-    "https://plankton-app-znmwk.ondigitalocean.app/"
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8080",
+#     "http://localhost:3000",
+#     "http://24.144.76.8:8080",
+#     "https://plankton-app-znmwk.ondigitalocean.app/"
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
