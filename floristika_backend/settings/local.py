49e47db1cc68b45f4c6f99a0a7a8849ff1e4ae68
mike <mike@cdontiy.com>
2024-04-16 17:02:12 +0300

    fixed trusted

--- floristika_backend/settings/local.py
+++ floristika_backend/settings/local.py
@@ -13,7 +13,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://localhost:3000",
-    "https://24.144.76.8:8080"
+    "http://24.144.76.8:8080"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
